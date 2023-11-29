c27dd0d30e3f903e01c0f1adb279024056ddc395
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-11-29 15:00:41 -0300

    Add Google Fonts and enhanced theme properties in TailwindCSS config
    This commits the addition of Google Font "Hanken Grotesk" to the index.html. This is done by a utilization of a CDN (Content Delivery Network) link, allowing for more flexible and diverse font choices. Concurrently, there are changes in the tailwind.config.js where theme properties have been extended. This extends the available color palette while specifying the newly added font-family which gives more scope for design choice and boosts the design consistency throughout the website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
     content: ['./src/**/*.js', './**/*.html'], // no caso dos projetos do frontend mentor
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    light_red: 'hsl(0, 100%, 67%)',
+                    orangey_yellow: 'hsl(39, 100%, 56%)',
+                    Green_teal: 'hsl(166, 100%, 37%)',
+                    Cobalt_blue: 'hsl(234, 85%, 45%)',
+                },
+                gradients: {
+                    light_slate_blue: 'hsl(252, 100%, 67%)',
+                    light_royal_blue: 'hsl(241, 81%, 54%)',
+                    violet_blue: 'hsla(256, 72%, 46%, 1)',
+                    persian_blue: 'hsla(241, 72%, 46%, 0)',
+                },
+                neutral: {
+                    white: 'hsl(0, 0%, 100%)',
+                    pale_blue: 'hsl(221, 100%, 96%)',
+                    light_lavender: 'hsl(241, 100%, 89%)',
+                    dark_gray_blue: 'hsl(224, 30%, 27%)',
+                },
+            },
+            fontFamily: {
+                hanken_grotesk: ['Hanken Grotesk', 'sans-serif'],
+            },
+            fontSize: {
+                body: '18px',
+            },
+        },
     },
     plugins: [],
 };
