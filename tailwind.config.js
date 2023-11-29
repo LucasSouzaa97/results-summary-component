3f44ce42a8624bd96286a414b41f89188faddee1
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-11-29 14:24:30 -0300

    Add tailwindcss and postcss support
    A new implementation of tailwindcss and postcss has been made for better front-end design. This change will mainly benefit projects from frontend mentor as indicated in the comment in tailwind.config.js file. Dependencies for tailwindcss and postcss, among others, have been added to the package-lock.json file as well.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.js', './**/*.html'], // no caso dos projetos do frontend mentor
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
